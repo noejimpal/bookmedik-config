37ba83d19e85d51c63414aa16f34889e4a271df3
noejimpal <noejimpal@alu.edu.gva.es>
2025-10-22 09:20:14 +0000

    Configuración inicial BookMedik

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -1,24 +1,33 @@
 <?php
 class Database {
-	public static $db;
-	public static $con;
-	
-	function Database(){
-	}
+    public static $db;
+    public static $con;
 
-	function connect(){
-		$con = new mysqli("localhost", "user", "pass", "db");
-		$con->query("set sql_mode=''");
-		return $con;
-	}
+    public function __construct(){
+        $this->user = "bookmedik_user";   // Usuario de la base de datos
+        $this->pass = "1234";             // Contraseña de la base de datos
+        $this->host = "localhost";        // Host de la base de datos
+        $this->ddbb = "bookmedik";        // Nombre de la base de datos
+    }
 
-	public static function getCon(){
-		if(self::$con==null && self::$db==null){
-			self::$db = new Database();
-			self::$con = self::$db->connect();
-		}
-		return self::$con;
-	}
-	
+    public function connect(){
+        $con = new mysqli($this->host, $this->user, $this->pass, $this->ddbb);
+
+        if ($con->connect_error) {
+            die("Connection failed: " . $con->connect_error);
+        }
+
+        $con->query("SET NAMES utf8"); // Configuración de codificación
+        return $con;
+    }
+
+    public static function getCon(){
+        if(self::$con == null && self::$db == null){
+            self::$db = new Database();
+            self::$con = self::$db->connect();
+        }
+        return self::$con;
+    }
 }
 ?>
+
